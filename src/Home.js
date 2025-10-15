f34da8b4571bce0495d0d6044a25ea7abcd4994c
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-10-15 12:11:09 +0200

    feat: stampa lista blog

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 const Home = () => {
-    return (  
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+
+
+    return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((curBlog) => (
+                <div className="blog-preview" key={curBlog.id}>
+                    <h2>{curBlog.title}</h2>
+                    <p>Written by {curBlog.body}</p>
+                </div>
+            ))}
         </div>
     );
 }
- 
+
 export default Home;
