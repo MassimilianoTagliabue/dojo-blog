c707e2d85b60149a1de63b20383ba0c1de610ba8
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-10-22 13:17:46 +0200

    feat: messaggio di caricamento

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,14 @@
-
 const BlogList = ({blogs,title}) => {
     
     
-
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((curBlog) => (
                 <div className="blog-preview" key={curBlog.id}>
                     <h2>{curBlog.title}</h2>
-                    <p>Written by {curBlog.body}</p>
+                    <p>Written by {curBlog.author}</p>
+                    
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    //prendo i dati dall'API e li inserisco nello state blogs
+    //prima di richiedere i dati, devo avviare il server (json-server --watch data/db.json --port 8000)
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => res.json())
+            .then(data => {
+                setBlogs(data)
+                setIsLoading(false);    //quando ho i dati, non viene più mostrato il messaggio di caricamento
+            });
+
+    });
 
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs"/>
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+
         </div>
     );
 }
